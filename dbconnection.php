26ed25fabfd95e122c5518828195ff4bbedda134
Thắng20cm <85218722+thang20cm@users.noreply.github.com>
2023-08-09 15:16:45 +0700

    Update dbconnection.php

--- dbconnection.php
+++ dbconnection.php
@@ -1,7 +1,11 @@
 <?php
 
 function dbconnection(){
-    $con = mysqli_connect("localhost","root","","flutter_app");
+    $server = "localhost";
+    $username= "id21127401_buffquat13";
+    $password= "Thang30112002@";
+    $dbname="id21127401_buffquat13";
+    $con = mysqli_connect($server, $username, $password, $dbname);
     return $con;
 }
 
